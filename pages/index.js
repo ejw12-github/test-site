0ba00b1f0e44205693dcc6c8bbb4783970a9f260
ejw12-github <everettjw23@gmail.com>
2025-06-30 18:15:35 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
       <h3>Currently in development... please return later!</h3>
       <h3>If you want to test it, go to ftcmetrics.vercel.app/teams/(your team number)</h3>
       <h3>Bug tracker coming soon!</h3>
+      <h3>To see a full list of upcoming features, go to the <a href="/features">Upcoming Features List</a>
       <h5>This website currently uses the <a href="https://ftcscout.org/api">FTCScout API</a>. This will be switched to the <a href="https://ftc-events.firstinspires.org/api-docs/index.html">FIRST API</a> soon.</h5>
     </div>
   );
