85d7950d4d1c91dc85ecf028c12196b2e3af9c86
ejw12-github <everettjw23@gmail.com>
2025-06-30 12:47:00 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to the Teams Site</h1>
+      <p>Go to /teams/123 to see it in action!</p>
+    </div>
+  );
+}
