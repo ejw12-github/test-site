b854f16b39bdb385136c7a61724ebdac08d14150
ejw12-github <everettjw23@gmail.com>
2025-06-30 18:04:23 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to FTC Scouting App</h1>
-      <p>Try visiting <code>/teams/12345</code> to view a team's total OPR.</p>
+    <div style={{ textAlign: "center", marginTop: "50px", text-align: "center" }}>
+      <h1>FTCMetrics</h1>
+      <h3>Currently in development... please return later!</h3>
+      <h3>If you want to test it, go to ftcmetrics.vercel.app/teams/(your team number)</h3>
+      <h3>Bug tracker coming soon!</h3>
+      <h5>This website currently uses the FTCScout API (ftcscout.org/api). This will be switched to the FIRST API soon (ftc-events.firstinspires.org/api-docs/index.html).</h5>
     </div>
   );
 }
